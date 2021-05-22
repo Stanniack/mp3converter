c5cfda8b6457cfce0aa948381b39ef18950af39b
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-05-22 01:10:48 -0300

    Implementando função de request

--- ytdl.js
+++ ytdl.js
@@ -1,36 +1,46 @@
 const ytdl = require('youtube-dl-exec')
 
-ytdl('https://youtube.com/watch?v=0aX3Bjzdq0Y', {
-    dumpSingleJson: true,
-    noWarnings: true,
-    noCallHome: true,
-    noCheckCertificate: true,
-    preferFreeFormats: true,
-    youtubeSkipDashManifest: true,
-    referer: 'https://youtube.com/watch?v=0aX3Bjzdq0Y'
-})
-.then(output => {
-    let array = output['formats']
+function requestVideo (url) {
     let videoList = []
-    let video
 
-    for (item in array) {
-
-        if (array[item]['ext'] == 'webm' && array[item]['filesize'] != null) {
-            video = {
-                filesize: array[item]['filesize'],
-                format: array[item]['ext'],
-                videoUrl: array[item]['url']
+    ytdl(url, {
+        dumpSingleJson: true,
+        noWarnings: true,
+        noCallHome: true,
+        noCheckCertificate: true,
+        preferFreeFormats: true,
+        youtubeSkipDashManifest: true,
+        referer: url
+    })
+    .then(output => {
+        let array = output['formats']
+        let video
+    
+        for (item in array) {
+    
+            if (array[item]['ext'] == 'webm' && array[item]['filesize'] != null) {
+                video = {
+                    filesize: array[item]['filesize'],
+                    format: array[item]['ext'],
+                    videoUrl: array[item]['url']
+                }
+    
+                videoList.push(video)
             }
-
-            videoList.push(video)
+    
         }
+    
+        /* Ordena por tamanho filesize em ordem crescente*/
+        videoList.sort((video, anotherVideo) => video.filesize < anotherVideo.filesize ? -1 : (video.filesize > anotherVideo.filesize ? 1 : 0))
 
-    }
+        //videoList.forEach(item => console.log(item))
 
-    /* Ordena por tamanho filesize em ordem crescente*/
-    videoList.sort((video, anotherVideo) => video.filesize < anotherVideo.filesize ? -1 : (video.filesize > anotherVideo.filesize ? 1 : 0))
+                 
+    }).catch(err => console.log('Erro ao fazer a requisição do vídeo: ' + err))
 
-    videoList.forEach(item => console.log(item))
-        
-}).catch(err => console.log('Erro ao fazer a requisição do vídeo: ' + err))
+    return videoList
+
+}
+
+let videoList = requestVideo('https://www.youtube.com/watch?v=Rb33hto3d7g')
+
