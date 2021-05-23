dc979663658f7d93afdc1b280877c3e9aaf406f5
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-05-22 23:02:10 -0300

    Implementação inicial finalizada

--- ytdl.js
+++ ytdl.js
@@ -1,4 +1,6 @@
+const request = require('request')
 const ytdl = require('youtube-dl-exec')
+const fs = require('fs');
 
 function requestVideo (url) {
     let videoList = []
@@ -33,14 +35,15 @@ function requestVideo (url) {
         /* Ordena por tamanho filesize em ordem crescente*/
         videoList.sort((video, anotherVideo) => video.filesize < anotherVideo.filesize ? -1 : (video.filesize > anotherVideo.filesize ? 1 : 0))
 
-        //videoList.forEach(item => console.log(item))
-
+        return videoList
                  
-    }).catch(err => console.log('Erro ao fazer a requisição do vídeo: ' + err))
-
-    return videoList
+    })
+    .then(videoList => {
+        //console.log(videoList[0]['videoUrl'])
+        request(videoList[0]['videoUrl']).pipe(fs.createWriteStream('audio.mp3'))
+    })
+    .catch(err => console.log('Erro ao fazer a requisição do vídeo: ' + err))
 
 }
 
-let videoList = requestVideo('https://www.youtube.com/watch?v=Rb33hto3d7g')
-
+requestVideo('https://www.youtube.com/watch?v=L_jWHffIx5E')
