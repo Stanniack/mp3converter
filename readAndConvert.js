820bba93910a325b8684141b3520eda95326d901
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-09-23 22:57:37 -0300

    Mostrando erro

--- readAndConvert.js
+++ readAndConvert.js
@@ -6,7 +6,7 @@ const sleep = require('./sleep.js')
 fs.readdir(dir, (err, paths) => {
     if (!err){
         for (i in paths) {
-            convert(`${dir}/${paths[i]}`, `./converted/${paths[i]}.mp3`, err => console.log(`Falha ao tentar converter ${paths[i]} para .mp3`))
+            convert(`${dir}/${paths[i]}`, `./converted/${paths[i]}.mp3`, err => console.log(`Falha ao tentar converter ${paths[i]} para .mp3: ` + err))
         }      
 
     } else
