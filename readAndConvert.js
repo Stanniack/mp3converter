415c9df346784f27534a63d349e71c327d76de38
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-09-26 08:18:38 -0300

    Refatoração

--- readAndConvert.js
+++ readAndConvert.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
 const dir = './download'
-const {convert, convert2, doConversion} = require('./videoConverterToMp3.js')
-const sleep = require('./sleep.js')
+const { convert, convert2} = require('./videoConverterToMp3.js')
 
-fs.readdir(dir, (err, paths) => {
-    if (!err){
-        for (i in paths) {
-            convert(`${dir}/${paths[i]}`, `./converted/${paths[i]}.mp3`, err => console.log(`Falha ao tentar converter ${paths[i]} para .mp3: ` + err))
-        }      
+function convertToMP3() {
+    fs.readdir(dir, (err, paths) => {
+        if (!err) {
+            for (i in paths) {
+                convert2(`${dir}/${paths[i]}`, `./converted/${paths[i]}.mp3`, err => console.log(`Falha ao tentar converter ${paths[i]} para .mp3: ` + err))
+            }
 
-    } else
-        console.log(err)
-})
+        } else
+            console.log(err)
+    })
+
+}
+
+convertToMP3();
+
+
--- videoConverterToMp3.js
+++ videoConverterToMp3.js
@@ -11,27 +11,6 @@ function convert(input, output, callback) {
         .run()
 }
 
-function convert2 (input, output) {
-    return new Promise ((resolve, reject) => {
-        try {
-            ffmpeg(input)
-            .output(output)
-            .on('end', () => console.log('conversão finalizada para ' + input))
-            .run()
-            resolve('Conversão bem sucedida.')
-        } catch (error) {
-            reject('Conversão mal sucedida: ' + error)
-        }
-    })
-}
-
-async function doConversion (input, output) {
-    await convert2(input, output)
-}
-
 module.exports = {
     convert: convert,
-    convert2: convert2,
-    doConversion: doConversion
-
 }
